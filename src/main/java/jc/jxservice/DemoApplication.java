233b7be0351ee136051b377bf6399ed38895fdb9
Jason Carreira <jason.carreira@perspectivepartners.com>
2018-05-07 14:36:31 -0400

    WIP

--- src/main/java/jc/jxservice/DemoApplication.java
+++ src/main/java/jc/jxservice/DemoApplication.java
@@ -1,18 +1,10 @@
 package jc.jxservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DemoApplication {
-
-  @Autowired
-  private DiscoveryClient discoveryClient;
-
 
   public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
